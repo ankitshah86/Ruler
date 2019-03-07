f33056316c464ebcb11d2a06e44216f44f9eee8f
Ankit <ankit.r.shah63@gmail.com>
2019-03-06 22:18:37 -0500

    Transparency Adjustment through slider now working

--- ruler.cpp
+++ ruler.cpp
@@ -5,22 +5,51 @@
 #include <iostream>
 #include <QDebug>
 
+
+
 using namespace  std;
 
 Ruler::Ruler()
 {
-    setWindowOpacity(0.7);
+    setWindowOpacity(1);
 
-    qDebug()<<this->logicalDpiX()<<this->physicalDpiX();
 
-    // setWindowFlags(Qt::FramelessWindowHint);
+    this->setMinimumHeight(QApplication::desktop()->screen()->height() * 0.8);
+    this->setMinimumWidth(200);
+    opacitySlider = new QSlider();
+    opacitySlider->setRange(1,100);
+    opacitySlider->setMaximumHeight(100);
+    opacitySlider->setToolTip("Adjust Transperancy");
+
+
+    QVBoxLayout *v = new QVBoxLayout(this);
+    v->setAlignment(Qt::AlignHCenter);
+    v->addWidget(opacitySlider,Qt::AlignHCenter);
+    this->setLayout(v);
+
+
+    //connect()
+    //connect(opacitySlider,SIGNAL(valueChanged(int),this,SLOT(resetOpacity(int)));
+  qDebug()<<this->logicalDpiX()<<this->physicalDpiX();
+
+
+  connect(opacitySlider,SIGNAL(valueChanged(int)),this,SLOT(resetOpacity(int)));
+   //  setWindowFlags(Qt::FramelessWindowHint);
    /* setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
     setAttribute(Qt::WA_TransparentForMouseEvents);
     this->setWindowTitle("Scale");
     setStyleSheet("background-color: rgba(0,0,0,0)");*/
 
+  }
+
+void Ruler::resetOpacity(int op)
+{
+    qDebug()<<op;
+    this->setWindowOpacity(1 - op/120.0);
 }
+
+
 
 void Ruler::paintEvent(QPaintEvent *event)
 {
@@ -39,15 +68,12 @@ void Ruler::paintEvent(QPaintEvent *event)
     this->setMaximumWidth(this->logicalDpiX()*2);
 
     QPainter painter(this);
+
     QPen p;
     p.setWidth(1);
 
     painter.setPen(p);
 
-
-
-    int TotalCm = 2.54 * this->height()/ double(this->physicalDpiY());
-    int TotalPixelsPerCm = double(this->physicalDpiY())/2.54;
 
 
     int h = 0;
@@ -81,5 +107,27 @@ void Ruler::paintEvent(QPaintEvent *event)
 
         i++;
 
+    }
+
+}
+
+void Ruler::mousePressEvent(QMouseEvent *event)
+{
+    qDebug()<<event->pos();
+    isMousePressed = true;
+    mousePressX = event->x();
+    mousePressY = event->y();
+}
+
+void Ruler::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event)
+    isMousePressed = false;
+}
+
+void Ruler::mouseMoveEvent(QMouseEvent *event)
+{
+    if(isMousePressed){
+        move(event->globalX() - mousePressX,event->globalY() -mousePressY);
     }
 }
--- ruler.h
+++ ruler.h
@@ -1,15 +1,30 @@
 #ifndef RULER_H
 #define RULER_H
 
-#include <QDialog>
+
 #include <QPaintEvent>
+#include <QtWidgets>
+
 
 class Ruler : public QDialog
 {
+    Q_OBJECT
+
+
+
 public:
     Ruler();
+    QSlider *opacitySlider;
+    bool isMousePressed = false;
+    int mousePressX,mousePressY;
+
+public slots:
+    void resetOpacity(int op);
 protected:
     void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
 };
 
 #endif // RULER_H
